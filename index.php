c10bdb22cbaf17a99eb4a0ebd4cb94fbf095ea3e
FrlFrz <farel.purnomosasi20@gmail.com>
2021-07-06 10:57:23 +0700

    Menambahkan list di file

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, quae aut repellendus magnam dolorem laboriosam ab officiis inventore quam. Explicabo id quas tempora suscipit magni vero debitis rerum natus eius.</p>
+    <ul>
+        <li>GG</li>
+        <li>Gemink</li>
+    </ul>
     
 </body>
 </html>
